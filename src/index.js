ee09caeec5f1683108639ebba050dbdd84299cf9
Alexandre Ferraz <alexandrehpf@hotmail.com>
2022-12-07 10:41:53 -0300

    Subindo os arquivos

---
+++ src/index.js
@@ -0,0 +1,30 @@
+// // Arquivo SASS
+// import './scss/index.scss'
+
+
+
+// const teste = () => {
+    
+//     const time = setTimeout(() => Promise.resolve('Teste Promisse'), 3000);
+
+//     return time;
+
+//     }
+// async function getData() {
+//     try {
+//         console.log('Primeiro');
+//         const data = await teste();
+//         console.log(data)
+//         console.log('Segundo');
+//     } catch (error){
+//         console.log('Aqui está o erro:' + error)
+//     }
+// }
+
+// getData();
+
+console.log("1");
+setTimeout(a => console.log('2'),3000);
+console.log("3");
+setTimeout(a => console.log('4'),1000);
+// setTimeout(function(){console.log("4");},1000);
