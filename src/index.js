59ffe2a943c0eca2306b318b77205f775d9a3f5c
Alexandre Ferraz <alexandrehpf@hotmail.com>
2022-12-07 15:40:28 -0300

    Finalizando o projeto

--- src/index.js
+++ src/index.js
@@ -1,4 +1,14 @@
 // // Arquivo SASS
 import './scss/index.scss'
 
+// Dependeias
+import 'jquery'
+import 'bootstrap'
+
+// Meus arquivos js
+
+import './js/core/includes'
+import './js/plugins/cityButtons'
+
+
 
